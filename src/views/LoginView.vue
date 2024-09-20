fbbca8b3167c1b2bb3bb530d56060a4c135547fe
layla7 <laylamouat04@gmail.com>
2024-09-20 11:13:02 +0100

    login and register screens

---
+++ src/views/LoginView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router"
+
+const accountNo = ref(undefined);
+const router = useRouter();
+
+function submit(){
+    if (accountNo.value == 3) {
+        router.push({path : "/"})
+    }
+}
+
+function register(){
+    router.push({path : "/register"})
+}
+</script>
+
+
+<template>
+    <div class="d-flex flex-column min-vh-100 min-vw-100">
+        <div class="d-flex flex-grow-1 justify-content-center align-items-center">
+            <div style = "color : #F3F9E3; font-family: 'Noto Sans', sans-serif;">
+                    <img src="Logo.png" alt="Placeholder" class="pb-5">
+                    <h1 class = "text-light">Login</h1>
+                    <input type="text" class = "rounded" v-model = "accountNo" placeholder="Account Number"></input><br>
+                    <div @click="submit" class="btn btn-light mt-4">Go</div><br>
+                    <div @click="register" class="btn btn-light mt-4">Register Account</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+:global(body) {
+  background-color: #306E36;
+}
+</style>
