2fd8451fabf0e4d7a681c12142bbb7ba2178fb37
Joshua Price <git@jprice.uk>
2024-09-27 15:36:59 +0100

    Add create counter table script
    This is to initialise the accounts counter table with a default value
    starting from after the imported account numbers start.

---
+++ api/create-counter-table.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+
+csv_file_path = "industrial_project_dataset.csv"
+table_name = "qmbank-accounts-counter"
+
+dynamodb = boto3.resource(
+    "dynamodb",
+    region_name="eu-west-2",
+    aws_access_key_id=os.environ.get("ACCESS_KEY"),
+    aws_secret_access_key=os.environ.get("SECRET_KEY"),
+)
+table = dynamodb.Table(table_name)
+
+item = {"counter": 69}
+
+table.put_item(Item=item)
