aaacfd352d3471b70bc4e8d15fffd1c1ac782b14
Joshua Price <git@jprice.uk>
2024-09-28 22:53:00 +0100

    Autoformat store.js

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useStore = defineStore('store', () => {
-  const accountNo = ref(null)
+export const useStore = defineStore("store", () => {
+  const accountNo = ref(null);
 
   return {
     accountNo,
-  }
-})
+  };
+});
