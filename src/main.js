f4bd5a28e4fb89bc9d4122684ca54ef7e80ba154
Joshua Price <git@jprice.uk>
2024-09-28 21:36:24 +0100

    Add pinia to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import App from './App.vue'
-import router from './router'
 
-createApp(App).use(router).mount('#app')
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
+import App from './App.vue'
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+app.mount('#app')
