6cf61409a5786141c46dce20536fcc1249f9f156
Joshua Price <git@jprice.uk>
2024-09-28 20:29:47 +0100

    Rename vue router kebab-case tags to pascal case

--- src/components/TransactionCard.vue
+++ src/components/TransactionCard.vue
@@ -22,7 +22,7 @@ const props = defineProps({
           <h2>£{{ props.info.amount || 0 }}</h2>
           <h6>Exp: {{ props.info.experience || 0 }}</h6>
           <h6>{{ props.info.date || "Date Unknown" }}</h6>
-          <router-link to="/moreinfo" class="card-link">More Information</router-link>
+          <RouterLink to="/moreinfo" class="card-link">More Information</RouterLink>
         </div>
     </div>
 </template>
