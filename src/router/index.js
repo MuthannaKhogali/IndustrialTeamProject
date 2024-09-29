525c023c839e262a49a2aa1ca41fc650c0c9fa5c
Joshua Price <git@jprice.uk>
2024-09-29 01:01:24 +0100

    Add authentication guards on all logged in pages

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import PayeeDetails from "@/views/PayeeDetails.vue";
 import ScoreView from "@/views/ScoreView.vue";
+import { useStore } from "@/store";
 
 const routes = [
   {
@@ -52,6 +53,24 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+function isAuthenticated() {
+  const store = useStore();
+
+  return store.accountNo === store.accountInfo?.account_id;
+}
+
+router.beforeEach(async (to) => {
+  if (
+    !isAuthenticated() &&
+    // Avoid an infinite redirect
+    to.name !== "login" &&
+    to.name !== "register"
+  ) {
+    // redirect the user to the login page
+    return { name: "login" };
+  }
 });
 
 export default router;
