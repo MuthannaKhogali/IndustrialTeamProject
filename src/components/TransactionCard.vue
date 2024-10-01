3ac24fedca55122eae841dc94b89a29d49da7a08
MuthannaKhogali <2457764@dundee.ac.uk>
2024-10-01 22:20:40 +0100

    Minus and Plus added coresspondingly beside pounds + tells you senders name

--- src/components/TransactionCard.vue
+++ src/components/TransactionCard.vue
@@ -7,16 +7,15 @@ const props = defineProps({
     required: true,
   }
 });
-
 </script>
 
 <template>
   <div class="company card rounded-0">
     <div class="card-body">
       <div class="card-title">
-        <h1>{{ props.info.recipient_name || "Name Undefined" }}</h1>
+        <h1>{{ props.info.is_outgoing ? props.info.recipient_name : props.info.sender_name || "Name Undefined" }}</h1>
       </div>
-      <h2>£{{ props.info.amount / 100 || 0 }}</h2>
+      <h2>{{ props.info.is_outgoing ? '-' : '+' }}£{{ props.info.amount / 100 || 0 }}</h2>
       <h6>Exp: {{ props.info.score || 0 }}</h6>
       <h6>{{ props.info.date || "Date Unknown" }}</h6>
       <h6>Reference: {{ props.info.reference || "No Reference" }}</h6>
@@ -34,7 +33,7 @@ const props = defineProps({
   text-align: left;
   border-style: solid;
   border-width: 0px 0px 0px 35px;
-  border-color: v-bind("info.colour");
+  border-color: v-bind("props.info.colour");
   border-radius: 20px;
 }
 </style>
