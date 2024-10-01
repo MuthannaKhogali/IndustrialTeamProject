971ffa6793c676f550b629f9d0c82bedf8f9529f
MuthannaKhogali <2457764@dundee.ac.uk>
2024-10-01 23:48:59 +0100

    removed time/changed .score to .experience

--- src/components/TransactionCard.vue
+++ src/components/TransactionCard.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 //this was referenced https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
 //reference for hour and minute if needed https://www.codu.co/articles/how-to-show-hours-and-minutes-only-with-tolocaletimestring-7dskbbzo 
 function makeDate(d) {
-  const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12:true};
+  const options = { year: 'numeric', month: 'long', day: 'numeric'};
   return new Date(d).toLocaleDateString(undefined, options);
 }
 
@@ -24,7 +24,7 @@ function makeDate(d) {
         <h1>{{ props.info.is_outgoing ? props.info.recipient_name : props.info.sender_name || "Name Undefined" }}</h1>
       </div>
       <h2 :class="props.info.is_outgoing ? 'minus' : 'plus'">{{ props.info.is_outgoing ? '-' : '+' }}£{{ (props.info.amount / 100).toFixed(2) || 0 }}</h2>
-      <h6>Exp: {{ props.info.score || 0 }}</h6>
+      <h6>Exp: {{ props.info.experience || 0 }}</h6>
       <h6>{{ makeDate(props.info.date) || "Date Unknown" }}</h6>
       <h6>Reference: {{ props.info.reference || "No Reference" }}</h6>
       <RouterLink to="/moreinfo" class="card-link">More Information</RouterLink>
