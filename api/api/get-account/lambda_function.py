1b634d6dde964bcf14f4689e47b248d6940781ae
Joshua Price <git@jprice.uk>
2024-10-02 23:53:07 +0100

    Return user's level

--- api/api/get-account/lambda_function.py
+++ api/api/get-account/lambda_function.py
@@ -4,6 +4,21 @@ client = boto3.client("dynamodb")
 tableName = "qmbank-accounts"
 
 
+# Calculates user level based on current XP. Max level of 10.
+# user_experience is the user's current XP, x & y represent difficulty.
+# Level boundaries are calculated with (level / x)^y, rounded to nearest whole number, then multiplied by 100 to reflect our XP score.
+# Returns user's level.
+def calculate_user_level(user_experience, x=0.35, y=1.5):
+    max_level = 10
+    for level in range(1, max_level + 1):
+        level_boundary = round((level / x) ** y * 100)
+        if user_experience < level_boundary:
+            return (
+                level - 1 if level > 0 else 0
+            )  # Return level 0 if no boundaries have been hit yet.
+    return max_level  # Return level 10 if XP exceeds all boundaries
+
+
 def lambda_handler(event, context):
     account_no = event["pathParameters"]["id"]
 
@@ -33,8 +48,7 @@ def lambda_handler(event, context):
         # response["alternatives"] =
     else:
         response["is_company"] = False
-        # TODO
-        response["level"] = 1
+        response["level"] = calculate_user_level(int(item.get("user_experience", {}).get("N", 0)))
         response["streak"] = 0
 
     return response
